403ae5798a731cfe46bcf3351fabf984781340a8
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2016-01-11 22:10:41 +0530

    some refactoring

--- Codec.js
+++ Codec.js
@@ -1,14 +1,8 @@
-
-/* 
- * Decodes any message received from the boot strap server
- * Input: a string (eg: length REGOK no_nodes IP_1 port_1 IP_2 port_2 )
- * Output fofrmat: a dictionary
- */
 var decodeResponse = function(strmsg) {
     var arrmsg = strmsg.split(" ");
     var dict = {};
     dict['type'] = arrmsg[1];
-    
+
     // JOIN does not have a ID
     if(dict.type != "JOIN")
         dict['ID'] = parseInt(arrmsg[2], 10);
@@ -34,18 +28,32 @@ var decodeResponse = function(strmsg) {
     }
     else if(dict.type == "SEROK"){
 
+    } else if(dict.type == "SER"){
+        dict['IP'] = arrmsg[2];
+        dict['PORT'] = parseInt(arrmsg[3]);
+        dict['fileName'] = arrmsg[4];
+        dict['qID'] = parseInt(arrmsg[5]);
+    } else if(dict.type == "DONE"){
+        dict['IPs'] = arrmsg[2];
+        dict['port'] = parseInt(arrmsg[3]);
+        dict['qID'] = parseInt(arrmsg[4]);
+    } else if(dict.type == "RESULT"){
+        dict['IP'] = arrmsg[2];
+        dict['PORT'] = parseInt(arrmsg[3]);
+        dict['qID'] = parseInt(arrmsg[4]);
+        dict['result'] = parseInt(arrmsg[5]);
     }
     return dict;
 };
 
 /*
- * Encodes in to a message to Bootstrap server 
+ * Encodes in to a message to Bootstrap server
  * Input: all strings (port can be a number)
  * Output: a string (eg: length UNREG IP_address port_no username)
  */
 var encodeMessage = function(type, IP, port, arg1, arg2){
-	var space  = " "
-	var spaces;
+    var space  = " "
+    var spaces;
     if(typeof port == 'number')
         port = port.toString();
     var msg;
@@ -64,10 +72,13 @@ var encodeMessage = function(type, IP, port, arg1, arg2){
         var msglen = 4 + spaces + type.length + IP.length + port.length;
         msg = type + space + IP + space + port;
     }
-	
-	var strmsg = msglen.toString();
-	while(strmsg.length < 4)
-		strmsg = "0".concat(strmsg);
-	var ret = strmsg.concat(space, msg);
-	return ret;
+
+    var strmsg = msglen.toString();
+    while(strmsg.length < 4)
+        strmsg = "0".concat(strmsg);
+    var ret = strmsg.concat(space, msg);
+    return ret;
 }
+
+exports.decodeResponse = decodeResponse;
+exports.encodeMessage = encodeMessage;
